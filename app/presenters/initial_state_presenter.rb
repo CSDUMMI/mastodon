7fa68e626c34dd0dd552280367fd12c1fd948d81
CSDUMMI <csdummi.misquality@simplelogin.co>
2022-11-12 00:10:35 +0100

    ADD REGISTRATIONS_REDIRECT_URI and OMNI_AUTH to initial state data type

--- app/presenters/initial_state_presenter.rb
+++ app/presenters/initial_state_presenter.rb
@@ -3,7 +3,8 @@
 class InitialStatePresenter < ActiveModelSerializers::Model
   attributes :settings, :push_subscription, :token,
              :current_account, :admin, :owner, :text, :visibility,
-             :disabled_account, :moved_to_account
+             :disabled_account, :moved_to_account,
+             :OMNIAUTH_ONLY, :REGISTRATIONS_REDIRECT_URI
 
   def role
     current_account&.user_role
