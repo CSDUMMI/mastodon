5c335b1f1fbdc57adaae065f8f6b1bbb928ee522
CSDUMMI <csdummi.misquality@simplelogin.co>
2022-11-12 19:49:08 +0100

    Move OMNIAUTH_ONLY and REGISTRATIONS_REDIRECT_URI into initial_state.meta

--- app/serializers/initial_state_serializer.rb
+++ app/serializers/initial_state_serializer.rb
@@ -6,7 +6,7 @@ class InitialStateSerializer < ActiveModel::Serializer
   attributes :meta, :compose, :accounts,
              :media_attachments, :settings,
              :max_toot_chars, :poll_limits,
-             :languages, :OMNIAUTH_ONLY, :REGISTRATIONS_REDIRECT_URI
+             :languages
 
   has_one :push_subscription, serializer: REST::WebPushSubscriptionSerializer
   has_one :role, serializer: REST::RoleSerializer
