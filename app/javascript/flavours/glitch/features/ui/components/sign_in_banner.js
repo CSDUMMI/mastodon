ff60fe4f5f565fb17f563f8a402b0438e4b01497
CSDUMMI <csdummi.misquality@simplelogin.co>
2022-11-13 19:51:18 +0100

    Revert changes made to sign_in_banner.js
    Helps identify which file is actually being used.

--- app/javascript/flavours/glitch/features/ui/components/sign_in_banner.js
+++ app/javascript/flavours/glitch/features/ui/components/sign_in_banner.js
@@ -14,14 +14,9 @@ const SignInBanner = () => {
 
   let signupButton;
 
-  console.log(OMNIAUTH_ONLY)
-  console.log(REGISTRATIONS_REDIRECT_URI)
-  console.log(signupLink)
-  console.log(signinLink)
-
   if (registrationsOpen) {
     signupButton = (
-      <a href='{signupLink}' className='button button--block button-tertiary'>
+      <a href='/auth/sign_up' className='button button--block button-tertiary'>
         <FormattedMessage id='sign_in_banner.create_account' defaultMessage='Create account' />
       </a>
     );
@@ -37,7 +32,7 @@ const SignInBanner = () => {
     <div className='sign-in-banner'>
 
       <p><FormattedMessage id='sign_in_banner.text' defaultMessage='Sign in to follow profiles or hashtags, favourite, share and reply to posts, or interact from your account on a different server.' /></p>
-      <a href='{signinLink}' className='button button--block'><FormattedMessage id='sign_in_banner.sign_in' defaultMessage='Sign in' /></a>
+      <a href='/auth/sign_in' className='button button--block'><FormattedMessage id='sign_in_banner.sign_in' defaultMessage='Sign in' /></a>
       {signupButton}
     </div>
   );
