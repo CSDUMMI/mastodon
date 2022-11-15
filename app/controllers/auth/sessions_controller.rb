1a9c9cd52e84a41219b21487bfd2387be6323ba4
CSDUMMI <csdummi.misquality@simplelogin.co>
2022-11-15 17:32:59 +0100

    Add redirect to OIDC logout page after logging out a user from mastodon

--- app/controllers/auth/sessions_controller.rb
+++ app/controllers/auth/sessions_controller.rb
@@ -40,8 +40,11 @@ class Auth::SessionsController < Devise::SessionsController
     session.delete(:challenge_passed_at)
     flash.delete(:notice)
 
-    store_location_for(:user, tmp_stored_location) if continue_after?
-
+    if ENV["OMNIAUTH_ONLY"] == 'true'
+      store_location_for(:user, ENV["
+    else
+      store_location_for(:user, tmp_stored_location) if continue_after?
+    end
   end
 
   def webauthn_options
