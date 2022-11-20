d1cedbbc4e44fe16cd4a8abf8d4cede567b586d0
CSDUMMI <csdummi.misquality@simplelogin.co>
2022-11-20 17:56:15 +0100

    Surrond JSX elements by <></>

--- app/javascript/flavours/glitch/features/ui/components/sign_in_banner.js
+++ app/javascript/flavours/glitch/features/ui/components/sign_in_banner.js
@@ -20,9 +20,11 @@ const SignInBanner = () => {
 
   if(OMNIAUTH_ONLY) {
     content = (
+      <>
       <a href='/auth/auth/openid_connect' className='button button--block button-tertiary'>
         <FormattedMessage id='sign_in_banner.sign_in_or_sign_up' defaultMessage="Login or Register" />
       </a>
+      </>
     );
   } else {
 
@@ -30,23 +32,29 @@ const SignInBanner = () => {
 
     if (registrationsOpen) {
       signupButton = (
+        <>
         <a href='/auth/sign_up' className='button button--block button-tertiary'>
           <FormattedMessage id='sign_in_banner.create_account' defaultMessage='Create account' />
         </a>
+        </>
       );
     } else {
       signupButton = (
+        <>
         <button className='button button--block button-tertiary' onClick={openClosedRegistrationsModal}>
           <FormattedMessage id='sign_in_banner.create_account' defaultMessage='Create account' />
         </button>
+        </>
       );
     }
 
     content = (
+        <>
         <a href='/auth/sign_in' className='button button--block'>
           <FormattedMessage id="sign_in_banner.sign_in" defaultMessage='Sign in' />
         </a>
         {signupButton}
+        </>
     );
   }
 
