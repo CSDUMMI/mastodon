36c2e1f1f9b97f9e658c37804afefef111c6b1cf
CSDUMMI <csdummi.misquality@simplelogin.co>
2022-11-23 13:22:41 +0100

    Call redirect_to

--- app/controllers/auth/sessions_controller.rb
+++ app/controllers/auth/sessions_controller.rb
@@ -42,8 +42,7 @@ class Auth::SessionsController < Devise::SessionsController
 
 
     if ENV["OMNIAUTH_ONLY"] == 'true'
-      store_location_for(:user, "/end-session-endpoint")
-      print(stored_location_for(:user))
+      redirect_to("/end-session-endpoint")
     else
       store_location_for(:user, tmp_stored_location) if continue_after?
     end
