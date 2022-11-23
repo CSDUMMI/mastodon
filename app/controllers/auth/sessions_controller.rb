caa2e19f168bfd54dc2390109b66628fdb1a95d9
CSDUMMI <csdummi.misquality@simplelogin.co>
2022-11-23 13:16:56 +0100

    Add logging

--- app/controllers/auth/sessions_controller.rb
+++ app/controllers/auth/sessions_controller.rb
@@ -43,6 +43,7 @@ class Auth::SessionsController < Devise::SessionsController
 
     if ENV["OMNIAUTH_ONLY"] == 'true'
       store_location_for(:user, "/end-session-endpoint")
+      print(stored_location_for(:user))
     else
       store_location_for(:user, tmp_stored_location) if continue_after?
     end
