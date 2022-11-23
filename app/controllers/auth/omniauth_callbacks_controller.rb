cc8adbc96e4338ee4fba5edc971652760a563748
CSDUMMI <csdummi.misquality@simplelogin.co>
2022-11-23 21:12:17 +0100

    Add logging

--- app/controllers/auth/omniauth_callbacks_controller.rb
+++ app/controllers/auth/omniauth_callbacks_controller.rb
@@ -36,6 +36,8 @@ class Auth::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
       last_uri = stored_location_for(:user)
 
+      print("Redirecting after sign in to " + last_uri)
+
       last_uri || root_path
 
     else
