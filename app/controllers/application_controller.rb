1fa9b4bca5905c801982ec436ab09dc104e11767
CSDUMMI <csdummi.misquality@simplelogin.co>
2022-11-25 22:58:21 +0100

    Redirect from the Account Settings to keycloak

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,6 +19,7 @@ class ApplicationController < ActionController::Base
   helper_method :single_user_mode?
   helper_method :use_seamless_external_login?
   helper_method :whitelist_mode?
+  helper_method :omniauth_only?
 
   rescue_from ActionController::ParameterMissing, Paperclip::AdapterRegistry::NoHandlerError, with: :bad_request
   rescue_from Mastodon::NotPermittedError, with: :forbidden
@@ -114,6 +115,14 @@ class ApplicationController < ActionController::Base
 
   def use_seamless_external_login?
     Devise.pam_authentication || Devise.ldap_authentication
+  end
+
+  def omniauth_only?
+    ENV["OMNIAUTH_ONLY"] == 'true'
+  end
+
+  def sso_account_settings
+    ENV["SSO_ACCOUNT_SETTINGS"]
   end
 
   def current_account
