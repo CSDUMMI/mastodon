544f94704c6bdea1b9657dc97e253dff63caf727
CSDUMMI <csdummi.misquality@simplelogin.co>
2023-02-05 15:48:56 +0100

    Increase rate limit on certain API calls to 2300

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -66,6 +66,7 @@ class Rack::Attack
     IpBlock.blocked?(req.remote_ip)
   end
 
+<<<<<<< HEAD
   throttle('throttle_authenticated_api', limit: 1_500, period: 5.minutes) do |req|
     req.authenticated_user_id if req.api_request?
   end
@@ -90,11 +91,11 @@ class Rack::Attack
     req.throttleable_remote_ip if req.post? && req.path == '/api/v1/accounts'
   end
 
-  throttle('throttle_authenticated_paging', limit: 300, period: 15.minutes) do |req|
+  throttle('throttle_authenticated_paging', limit: 2300, period: 15.minutes) do |req|
     req.authenticated_user_id if req.paging_request?
   end
 
-  throttle('throttle_unauthenticated_paging', limit: 300, period: 15.minutes) do |req|
+  throttle('throttle_unauthenticated_paging', limit: 2300, period: 15.minutes) do |req|
     req.throttleable_remote_ip if req.paging_request? && req.unauthenticated?
   end
 
